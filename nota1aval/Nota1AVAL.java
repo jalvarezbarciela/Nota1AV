2e294d79eca8c1f6968bda386c0d166a41114aac
jalvarezbarciela <56076790+jalvarezbarciela@users.noreply.github.com>
2019-12-12 13:31:29 +0100

    Ejercicio Nota 1 Trimestre

---
+++ nota1aval/Nota1AVAL.java
@@ -0,0 +1,17 @@
+package nota1aval;
+
+/**
+ *
+ * @author Jose
+ */
+public class Nota1AVAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculo a=new Calculo();
+        a.calcularNota();
+    }
+    
+}
